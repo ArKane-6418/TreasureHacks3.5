5fab36f5d26e73109c5b461ae113bc6493b17bc3
Jamie Zhao <rainyday.tea.party@gmail.com>
2023-04-29 23:39:52 -0400

    Frontend ready for backend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const endpoint = '';
+
 function App() {
   const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null);
   const [output, setOutput] = useState(null);
   const [awaitingOutput, setAwaitingOutput] = useState(false);
 
+  // Send to backend for processing.
   const handleSubmit = (event) => {
     event.preventDefault();
     // Frontend rejection if the image is still being processed.
@@ -15,12 +18,11 @@ function App() {
     }
 
     console.log("Image submitted:", image);
-
-    // Only for testing
-    setOutput(preview);
-
+    setAwaitingOutput(true);
+    fetchImage();
   };
 
+  // Update the image preview.
   const handleFileChange = (event) => {
     setImage(event.target.files[0]);
 
@@ -33,10 +35,33 @@ function App() {
 
   };
 
+  // Fetch the image from the backend.
+  const fetchImage = async () => {
+    // Backend rejection if the image is still being processed.
+    if (awaitingOutput) {
+      return;
+    }
+    if (!image) {
+      setAwaitingOutput(false);
+      return;
+    }
+    const res = await fetch(endpoint, {
+      method: "POST",
+      body: image,
+    });
+    const imageBlob = await res.blob();
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+    if (res.ok) {
+      setOutput(imageObjectURL);
+      setAwaitingOutput(false);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-shadow">Ha<span className="emphasis">loop</span>inate</h1>
+        <title>Haloopinate</title>
+        <h1 className="text-shadow">Ha<span className="emphasis anim1">l</span><span className="emphasis anim2">o</span><span className="emphasis anim3">o</span><span className="emphasis anim4">p</span>inate</h1>
         <h2>A deep-dream looping GIF generator</h2>
       </header>
       <form onSubmit={handleSubmit}>
@@ -50,6 +75,9 @@ function App() {
             <h3>Output</h3>
             <img className="output" src={output ? output : "https://via.placeholder.com/256"} alt="Output"/>
           </div>
+        </div>
+        <div>
+          <h5 className="notification"></h5>
         </div>
         <input type="file" accept="image/*" onChange={handleFileChange} />
         <button type="submit">Generate!</button>
